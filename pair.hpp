9a5c77e755713281a98c95bbf6d104e398f8d3de
Tristan Brindle <t.c.brindle@gmail.com>
2018-01-16 16:21:25 +0000

    Add exercise 2 solutions

--- pair.hpp
+++ pair.hpp
@@ -7,19 +7,46 @@
 template <typename T, typename U>
 class Pair {
 public:
-    Pair(const T& t, const U& u)
-        : first(t), second(u)
-    {}
+    Pair(const T& t, const U& u);
 
-    T& get_first() { return first; }
-    U& get_second() { return second; }
+    T& get_first();
+    U& get_second();
 
-    const T& get_first() const { return first; }
-    const U& get_second() const { return second; }
+    const T& get_first() const;
+    const U& get_second() const;
 
 private:
     T first;
     U second;
+};
+
+template <typename T, typename U>
+Pair<T, U>::Pair(const T& t, const U& u)
+    : first(t), second(u)
+{}
+
+template <typename T, typename U>
+T& Pair<T, U>::get_first()
+{
+    return first;
+};
+
+template <typename T, typename U>
+U& Pair<T, U>::get_second()
+{
+    return second;
+};
+
+template <typename T, typename U>
+const T& Pair<T, U>::get_first() const
+{
+    return first;
+};
+
+template <typename T, typename U>
+const U& Pair<T, U>::get_second() const
+{
+    return second;
 };
 
 template <typename T, typename U>
